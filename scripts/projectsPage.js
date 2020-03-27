9fd72ddb776abc617e55cbceaac61ec40e83ef05
Udit Jain <udit.jain@opscale.io>
2020-03-27 21:12:01 +0530

    add variable width of cards wrapper

--- scripts/projectsPage.js
+++ scripts/projectsPage.js
@@ -1,7 +1,14 @@
 const projectSectionWrapper = document.getElementById('projects__wrapper')
 
+const projectCardSection = document.getElementById('project__cards__wrapper')
+let gridColumnGap = 120 // will have to make it responsive
+
 try{
     const projectDetailData = loadElementsToArray('project-', projectSectionWrapper)
+
+    const projectCardData = loadElementsToArray('project__card-', projectCardSection)
+
+    projectCardSection.style.gridColumnGap = `${gridColumnGap}px`
 
     function loadQueriedProjectDetail(){
         const projectQuery = loadFromQuery()
@@ -12,7 +19,16 @@ try{
     }
     
     loadQueriedProjectDetail()
-    
+
+    let totalVisibleWidth = projectCardSection.clientWidth
+
+    if(projectCardData.valid){
+        projectCardData.items.forEach(card => {
+            if(card.children[0] && card.children[0].nodeName.toLowerCase() === 'img') {
+                card.children[0].style.width = `${(totalVisibleWidth / 3) - ((2/3)*gridColumnGap)}px` //  this will change on adding media query
+            }
+        })
+    }
 }catch(ex){
     console.log(ex)
 }
