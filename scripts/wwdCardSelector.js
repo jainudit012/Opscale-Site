e9d1b1e4f0be60fbefa141cec86c7947fb77b4e9
Udit Jain <udit.jain@opscale.io>
2020-04-20 18:21:20 +0530

    add pagination for mobile

--- scripts/wwdCardSelector.js
+++ scripts/wwdCardSelector.js
@@ -1,8 +1,21 @@
+const wwdWrapper = document.getElementById('wwd__card__box')
 try {
-    const wrapper = document.getElementById('wwd__card__box')
-    const allCardData = loadElementsToArray('wwd__card-', wrapper)
+    const allCardData = loadElementsToArray('wwd__card-', wwdWrapper)
+
+    const classConfig = {
+        frontClass: 'card__selected',
+        backClass: '',
+        nextClass: '',
+        disabledPaginationClass: 'mobile-paginator-disabled',
+        nextBtnId: 'wwd__paginator-2',
+        backBtnId: 'wwd__paginator-1',
+        slideAnimationClass: 'slideOut'
+    }
+
     if(allCardData.valid){
         addSelectItemListener(allCardData.items, 'card__selected', false)
+
+        otherPaginate(allCardData.items, classConfig, null)
     }
 }catch(err){
     console.log(err)
