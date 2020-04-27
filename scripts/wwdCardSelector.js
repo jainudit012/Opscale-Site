16e5e2a6a0f1314a7796c81209185a56f71c11a3
Udit Jain <udit.jain@opscale.io>
2020-04-27 13:52:19 +0530

    add swipe events for wwd card

--- scripts/wwdCardSelector.js
+++ scripts/wwdCardSelector.js
@@ -23,7 +23,14 @@ try {
 
     if(allCardData.valid){
         addSelectItemListener(allCardData.items, 'card__selected', false)
-
+        addSwipeEvents(allCardData.items, 
+            target => {
+                if(target.id.indexOf('1') !== -1) return;
+                wwdBackBtn.click() },
+            target => {
+                if(target.id.indexOf(allCardData.items.length) !== -1) return;
+                wwdNextBtn.click() }
+            )
         otherPaginate(allCardData.items, classConfig, null)
     }
 }catch(err){
